2123e402bd6d9d383f22cfcefba4eb823737983e
Rémi de Juvigny <remi.dejuvigny@strapi.io>
2022-02-04 17:15:52 +0100

    Basic SEO

--- packages/starters/gatsby-blog/starter/src/pages/articles/{StrapiArticle.slug}.js
+++ packages/starters/gatsby-blog/starter/src/pages/articles/{StrapiArticle.slug}.js
@@ -3,12 +3,20 @@ import Layout from "../../components/layout"
 import { graphql } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import BlocksRenderer from "../../components/blocks-renderer"
+import SEO from "../../components/seo"
 
 const ArticlePage = ({ data }) => {
   const article = data.strapiArticle
 
+  const seo = {
+    metaTitle: article.title,
+    metaDescription: article.description,
+    shareImage: article.cover,
+  }
+
   return (
     <Layout as="article">
+      <SEO seo={seo} />
       <header className="container max-w-4xl py-8">
         <h1 className="text-6xl font-bold text-neutral-700">{article.title}</h1>
         <p className="mt-4 text-2xl text-neutral-500">{article.description}</p>
@@ -38,6 +46,7 @@ export const query = graphql`
       cover {
         alternativeText
         localFile {
+          url
           childImageSharp {
             gatsbyImageData
           }
--- packages/starters/gatsby-blog/starter/src/pages/index.js
+++ packages/starters/gatsby-blog/starter/src/pages/index.js
@@ -2,22 +2,29 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Layout from "../components/layout"
 import ArticlesGrid from "../components/articles-grid"
+import SEO from "../components/seo"
 
 const IndexPage = () => {
-  const { allStrapiArticle } = useStaticQuery(graphql`
+  const { allStrapiArticle, strapiGlobal } = useStaticQuery(graphql`
     query {
       allStrapiArticle {
         nodes {
           ...ArticleCard
         }
       }
+      strapiGlobal {
+        siteName
+      }
     }
   `)
 
   return (
     <Layout>
+      <SEO />
       <header className="container mt-8">
-        <h1 className="text-6xl font-bold text-neutral-700">Website</h1>
+        <h1 className="text-6xl font-bold text-neutral-700">
+          {strapiGlobal.siteName}
+        </h1>
         <p className="mt-2 text-xl text-neutral-500">
           A cool blog made with Gatsby and Strapi v4
         </p>
