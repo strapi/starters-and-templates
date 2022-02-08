2660ed2a2614ec4c6dde3ee560d49f812689a17d
Mark Kaylor <mark.kaylor@strapi.io>
2022-02-08 12:46:47 +0100

    replace class with className

--- packages/starters/gatsby-blog/starter/src/components/navbar.js
+++ packages/starters/gatsby-blog/starter/src/components/navbar.js
@@ -3,13 +3,13 @@ import React from "react"
 
 const Navbar = () => {
   return (
-    <header class=" bg-primary-200">
-      <nav class="container flex flex-row items-baseline justify-between py-6">
-        <Link to="/" class="text-xl font-medium">
+    <header className="bg-primary-200">
+      <nav className="container flex flex-row items-baseline justify-between py-6">
+        <Link to="/" className="text-xl font-medium">
           Blog
         </Link>
-        <div class="flex flex-row items-baseline justify-end">
-          <Link class="font-medium" to="/about">
+        <div className="flex flex-row items-baseline justify-end">
+          <Link className="font-medium" to="/about">
             About
           </Link>
         </div>
